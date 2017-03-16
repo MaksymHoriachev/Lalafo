8d4b1a36f9e60352d62f88d7ad3c7f1d4d2dc7ca
Maxim Goryachev <maxto4kacom@gmail.com>
2017-03-16 10:18:19 +0200

    start

---
+++ web/js/main.js
@@ -0,0 +1,12 @@
+
+$('#language').click(function() {
+    $('#language-list').toggle();
+});
+
+$('#btn-close-purple').on('click', function () {
+    $('#notification-purple').fadeOut(0);
+});
+
+$('#btn-close-scarlet').on('click', function () {
+    $('#notification-scarlet').fadeOut(0);
+});
